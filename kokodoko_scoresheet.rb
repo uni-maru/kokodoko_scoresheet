f5d1e9e15f486acca04b0849db1d5933d6e0a1c1
uni-maru <umimaru3@gmail.com>
2017-01-11 19:46:19 +0900

    Markdownのtable記法

--- kokodoko_scoresheet.rb
+++ kokodoko_scoresheet.rb
@@ -324,6 +324,10 @@ if ARGV[1] == "sort" then
   }
 
 else
+  sep  = ARGV[1] == "table" ? '|' : ','
+  ends = ARGV[1] == "table" ? '|' : ''
+  puts sep+da.join(sep)+sep if ARGV[1] == "table"
+  puts (sep+"---")*da.size+sep if ARGV[1] == "table"
   #newer sort
   h.sort_by.with_index{|s,i|
     [s[1].values_at,i]
@@ -333,7 +337,7 @@ else
     #   s.gsub!(x,y)
     # }
     s = "'"+s if /^[0-9]+$/ =~ s
-    puts "\""+s+"\","+[k.values_at(*da),k.size].join(',')
+    puts ends+"\""+s+"\"#{sep}"+[k.values_at(*da),k.size].join(sep)+ends
   }
 
   #h.sort_by.with_index{|s,i|[s[1].values_at,i]}.map{|n,k|s=n.dup;rr.map{|x,y|s.gsub!(x,y)};puts "\""+s+"\","+[k.values_at(*da),k.size].join(',')}
