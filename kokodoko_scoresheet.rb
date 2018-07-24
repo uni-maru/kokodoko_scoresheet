eaeefb317bf1cefb9df2dbc4e6f54c33fe87e0a1
uni-maru <umimaru3@gmail.com>
2018-07-24 14:13:53 +0900

    第38回を追加

--- kokodoko_scoresheet.rb
+++ kokodoko_scoresheet.rb
@@ -48,6 +48,7 @@ file_list = {
   '第34回' => 'lists/kd_winner_34.txt',
   '第36回' => 'lists/kd_winner_36.txt',
   '第37回' => 'lists/kd_winner_37.txt',
+  '第38回' => 'lists/kd_winner_38.txt',
 }
 
 file_list.each {|no, fname|
